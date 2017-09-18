NamedKitten/Hexicord

commit a2695dab87ced937d58dc42a1df6661995e6927d

Huge includes reordering.
Now boost.beast and most of boost.asio only referenced from wss.cpp
and rest.cpp.

Why:

1. Allow to install to system directory without installing beast (in case
   of in-tree beast).

2. Don't make compiler parse dense boost templates more than necessary.

Before (1235de44c1cf5c00b95f23445705042f338bfd46):
    Command being timed: "ninja -j1 hexicord"
    User time (seconds): 16.40
    System time (seconds): 0.56
    Percent of CPU this job got: 99%
    Elapsed (wall clock) time (h:mm:ss or m:ss): 0:16.98
    ...
    Maximum resident set size (kbytes): 278932

    Command being timed: "ninja -j1 echo-bot"
    User time (seconds): 2.96
    System time (seconds): 0.12
    Percent of CPU this job got: 99%
    Elapsed (wall clock) time (h:mm:ss or m:ss): 0:03.09
    ...
    Maximum resident set size (kbytes): 206696

After (this commit):
    Command being timed: "ninja -j1 hexicord"
    User time (seconds): 14.17
    System time (seconds): 0.51
    Elapsed (wall clock) time (h:mm:ss or m:ss): 0:14.79
    ...
    Maximum resident set size (kbytes): 279460

    Command being timed: "ninja -j1 echo-bot"
    User time (seconds): 1.93
    System time (seconds): 0.09
    Percent of CPU this job got: 99%
    Elapsed (wall clock) time (h:mm:ss or m:ss): 0:02.05
    ...
    Maximum resident set size (kbytes): 140288

P.S. Used compiler: clang 4.0.1 from Arch Linux repo.

Difference when building library itself is not very big, but when
building something that uses library - memory usage is smaller a lot
because inclusion of gateway_client.hpp or rest_client.hpp does not
lead to inclusion of bloated boost headers anymore.

Also `make install` (installation to system directory) now supported again.

src/hexicord/rest_client.cpp
@@ -19,14 +19,15 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include <hexicord/rest_client.hpp>
+#include "hexicord/rest_client.hpp"
+
 #include <thread>                                     // std::this_thread::sleep_for
 #include <chrono>                                     // std::chrono::seconds, std::chrono::milliseconds
-#include <fstream>                                    // std::ifstream
-#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/asio/io_service.hpp>                  // boost::asio::io_service
 #include <boost/beast/http/error.hpp>                 // boost::beast::http::error::end_of_stream
-#include <hexicord/exceptions.hpp>
-#include <hexicord/internal/utils.hpp>                // Utils::getRatelimitDomain, Utils::domainFromUrl
+#include "hexicord/exceptions.hpp"
+#include "hexicord/internal/utils.hpp"                // Utils::getRatelimitDomain, Utils::domainFromUrl
+#include "hexicord/internal/rest.hpp"                 // Hexicord::REST
 
 #if defined(HEXICORD_DEBUG_LOG)
     #include <iostream>

src/hexicord/rest_client.hpp
@@ -22,16 +22,21 @@
 #ifndef HEXICORD_REST_CLIENT_HPP
 #define HEXICORD_REST_CLIENT_HPP
 
-#include <utility>
-#include <boost/asio/io_service.hpp>
-#include <boost/optional.hpp>
-#include <hexicord/permission.hpp>
-#include <hexicord/json.hpp>
-#include <hexicord/internal/rest.hpp>
-#include <hexicord/config.hpp>
-#include <hexicord/types.hpp>
+#include <cstdint>                      // uint8_t
+#include <utility>                      // std::pair
+#include <string>                       // std::string
+#include <vector>                       // std::vector
+#include <unordered_map>                // std::unordered_map
+#include <memory>                       // std::shared_ptr
+#include <boost/optional.hpp>           // boost::optional
+#include "hexicord/json.hpp"            // nlohamnn::json
+#include "hexicord/permission.hpp"      // Hexicord::Permissions
+#include "hexicord/config.hpp"          // HEXICORD_RATELIMIT_PREDICTION
+#include "hexicord/types.hpp"           // Hexicord::Snowflake, Hexicord::File, Hexicord::Image
+namespace boost { namespace asio { class io_service; }}
+namespace Hexicord { namespace REST { class HTTPSConnection; class MultipartEntity; class HTTPRequest; class HTTPResponse; }}
 #ifdef HEXICORD_RATELIMIT_PREDICTION
-    #include <hexicord/ratelimit_lock.hpp>
+    #include "hexicord/ratelimit_lock.hpp"
 #endif
 
 namespace Hexicord {
@@ -966,7 +971,9 @@ private:
 
         static inline REST::MultipartEntity fileToMultipartEntity(const File& file);
 
-        std::unique_ptr<REST::HTTPSConnection> restConnection;
+        // We have to use std::shared_ptr instead of std::unique_ptr because
+        // latter requires complete type but we forward-declare REST::HTTPSConnection.
+        std::shared_ptr<REST::HTTPSConnection> restConnection;
         boost::asio::io_service& ioService; // non-owning reference to I/O service.
     };
 }

CMakeLists.txt
@@ -225,13 +225,15 @@ target_include_directories(hexicord PUBLIC
                            ${PROJECT_BINARY_DIR})
 target_compile_options(hexicord PRIVATE ${WARNING_FLAGS})
 
-# NOT SUPPORTED until we remove references to beast from headers.
-#install(DIRECTORY ${CMAKE_SOURCE_DIR}/src/hexicord
-#        DESTINATION include
-#        FILES_MATCHING PATTERN "*.hpp")
-#install(TARGETS hexicord
-#        ARCHIVE DESTINATION lib
-#        LIBRARY DESTINATION lib)
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/src/hexicord
+        DESTINATION include
+        FILES_MATCHING
+         PATTERN "*.hpp"
+         PATTERN "internal" EXCLUDE)
+install(TARGETS hexicord
+        ARCHIVE DESTINATION lib
+        LIBRARY DESTINATION lib)
+
 
 option(HEXICORD_EXAMPLES "Build examples." OFF)
 

src/hexicord/event_dispatcher.cpp
@@ -19,8 +19,7 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include <hexicord/event_dispatcher.hpp>
-#include <iostream>
+#include "hexicord/event_dispatcher.hpp"
 
 namespace Hexicord {
     void EventDispatcher::addHandler(Event eventType, EventDispatcher::EventHandler handler) {

src/hexicord/event_dispatcher.hpp
@@ -22,8 +22,12 @@
 #ifndef HEXICORD_EVENTDISPATCHER_HPP
 #define HEXICORD_EVENTDISPATCHER_HPP 
 
-#include <unordered_map>
-#include <hexicord/json.hpp>
+#include <unordered_map>        // std::unordered_map
+#include <functional>           // std::function
+#include <cstddef>              // size_t
+#include <string>               // std::string
+#include <vector>               // std::vector
+#include "hexicord/json.hpp"    // nlohmann::json
 
 namespace Hexicord {
     enum class Event {

src/hexicord/exceptions.hpp
@@ -3,7 +3,6 @@
 
 #include <stdexcept>
 #include <string>
-#include <boost/system/system_error.hpp>
 
 /**
  * \file exceptions.hpp
@@ -12,8 +11,6 @@
  */
 
 namespace Hexicord {
-    using ConnectionError = boost::system::system_error;
-
     /// Base class for errors that can't be predicted in most cases.
     class RuntimeError : public std::runtime_error {
     public:

src/hexicord/gateway_client.cpp
@@ -19,17 +19,24 @@
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 // OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include <hexicord/gateway_client.hpp>
+#include "hexicord/gateway_client.hpp"
 
-#include <chrono>
-#include <hexicord/config.hpp>
-#include <hexicord/internal/utils.hpp>
+#include <cassert> // assert
+#include <chrono> // std::chrono::steady_clock
+#include <unordered_map> // std::unordered_map
+#include <boost/asio/error.hpp> // boost::asio::error
+#include <boost/asio/io_service.hpp> // boost::asio::io_service
+#include <boost/beast/websocket/error.hpp> // boost::beast::websocket::error
+#include "hexicord/config.hpp" // HEXICORD_ZLIB HEXICORD_DEBUG_LOG
+#include "hexicord/internal/wss.hpp" // Hexicord::TLSWebSocket
+#include "hexicord/internal/utils.hpp" // Hexicord::Utils::domainFromUrl
+
 #ifdef HEXICORD_ZLIB
-#include <hexicord/internal/zlib.hpp>
+    #include "hexicord/internal/zlib.hpp" // Hexicord::Zlib::decompress
 #endif
 
-#if defined(HEXICORD_DEBUG_LOG)
-    #include <iostream>
+#ifdef HEXICORD_DEBUG_LOG
+    #include <iostream> //
     #define DEBUG_MSG(msg) do { std::cerr <<  "gateway_client.cpp:" << __LINE__ << " " << (msg) << '\n'; } while (false)
 #else
     #define DEBUG_MSG(msg)

src/hexicord/gateway_client.hpp
@@ -22,13 +22,16 @@
 #ifndef HEXICORD_GATEWAY_CLIENT_HPP
 #define HEXICORD_GATEWAY_CLIENT_HPP
 
-#include <string>
-#include <vector>
-#include <boost/asio/io_service.hpp>
-#include <boost/asio/steady_timer.hpp>
-#include <hexicord/json.hpp>
-#include <hexicord/event_dispatcher.hpp>
-#include <hexicord/internal/wss.hpp>
+#include <cstdint>                       // uint8_t
+#include <memory>                        // std::unique_ptr
+#include <stdexcept>                     // std::runtime_error
+#include <string>                        // std::string
+#include <vector>                        // std::vector
+#include <boost/asio/steady_timer.hpp>   // boost::asio::steady_timer
+#include <hexicord/event_dispatcher.hpp> // Hexicord::Event, Hexicord::EventDispatcher
+#include <hexicord/json.hpp>             // nlohmann::json
+namespace Hexicord { class TLSWebSocket; }
+namespace boost { namespace asio { class io_service; } }
 
 namespace Hexicord {
     /**

commit f602b26c94c18eeeaa1c19f8eef070bee9a9a6a9

Fix build with HEXICORD_RATELIMIT_PREDICTION enabled.
a2695da broke it by removing definition of REST::HeadersMap from scope
of rest_client.hpp.

src/hexicord/rest_client.cpp
@@ -110,7 +110,7 @@ namespace Hexicord {
         nlohmann::json jsonResp = nlohmann::json::parse(response.body);
 
 #ifdef HEXICORD_RATELIMIT_PREDICTION
-        updateRatelimitsIfPresent(endpoint, response.headers);
+        updateRatelimitsIfPresent(endpoint, { response.headers.begin(), response.headers.end() });
 #endif
 
         if (response.statusCode / 100 != 2) {
@@ -752,7 +752,9 @@ namespace Hexicord {
     }
 
 #ifdef HEXICORD_RATELIMIT_PREDICTION 
-    void RestClient::updateRatelimitsIfPresent(const std::string& endpoint, const REST::HeadersMap& headers) {
+    void RestClient::updateRatelimitsIfPresent(const std::string& endpoint,
+                                               const std::unordered_map<std::string, std::string>& headers) {
+
         auto remainingIt = headers.find("X-RateLimit-Remaining");
         auto limitIt     = headers.find("X-RateLimit-Limit");
         auto resetIt     = headers.find("X-RateLimit-Reset");

src/hexicord/rest_client.hpp
@@ -983,7 +983,7 @@ private:
         void throwRestError(const REST::HTTPResponse& response, const nlohmann::json& payload);
 
 #ifdef HEXICORD_RATELIMIT_PREDICTION
-        void updateRatelimitsIfPresent(const std::string& endpoint, const REST::HeadersMap& headers);
+        void updateRatelimitsIfPresent(const std::string& endpoint, const std::unordered_map<std::string, std::string>& headers);
 #endif
 
         static inline REST::MultipartEntity fileToMultipartEntity(const File& file);